3ee04f4d1fcd946bdb34a24147f8d15ff13dfc73
Abhishek Malani <malani@gmail.com>
2020-10-17 21:49:26 -0700

    add scrollbar + save cfd settings

--- dotfiles/input.sh
+++ dotfiles/input.sh
@@ -1,6 +1,14 @@
 
+# Always show scrolbar
+defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
 
 # Keyboard repeat delay
 defaults write NSGlobalDomain InitialKeyRepeat -int 25
 defaults write NSGlobalDomain KeyRepeat -int 1
 
+# Expand save/print CFD by default 
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
